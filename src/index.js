9498eabc80ba201d5ff9eb7d6f7238d2df5e8f69
thamizgit <tothamizs4@gmail.com>
2023-05-01 13:49:44 +0530

    home and department

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Routes,
   Route,
 } from "react-router-dom";
@@ -13,9 +13,9 @@ root.render(
   <React.StrictMode>
     
     <Router>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
+
+        <App />
+
     </Router>
   </React.StrictMode>
 );
